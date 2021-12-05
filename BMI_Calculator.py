0b11ec7e1d7a93b7737e10e39071cedc58ad2c59
scottmgrossman <65791883+scottmgrossman@users.noreply.github.com>
2021-12-05 10:34:18 -0500

    Add files via upload

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
+# In[7]:
 
 
 from pywebio.input import *
@@ -9,17 +9,18 @@ from pywebio.output import *
 
 
 def bmicalculator():
-    height=input('Please enter your height inches ',type=FLOAT)
+    feet=input('Please enter how many feet tall you are',type=FLOAT)
+    inches=input('Now add inches',type=FLOAT)
     weight=input('Please enter your weight in pounds',type=FLOAT)
     
-    bmi=weight/(height/100)**2
+    bmi=(weight/(feet*12+inches)**2)*703
     
-    bmi_output=[(16,'Severely underweight'), (18.5, 'Underwight'),
+    bmi_output=[(16,'Severely Underweight'), (18.5, 'Underwight'),
                       (25,'Normal'),(30,'Overweight'),
-                      (35,'Moderately obese'),(float('inf'),'Severely obese')]
+                      (35,'Moderately Obese'),(float('inf'),'Severely Obese')]
     for tuple1,tuple2 in bmi_output:
         if bmi<=tuple1:
-            put_text('The BMI is: %.1f and the person is: %s' %(bmi,tuple2))
+            put_text('Your BMI is: %.1f and the person is: %s' %(bmi,tuple2))
             break
 
 if __name__=='__main__':
